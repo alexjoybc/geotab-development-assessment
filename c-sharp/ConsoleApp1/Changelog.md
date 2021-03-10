0a0fbf7d21d763ffc606675d2d2d300184188257
alex <alexandre.joyeux@nttdata.com>
2021-03-09 20:28:18 -0800

    removed nested if statements and improved flow
    - remove 3 level of nested statement, down to 3 levels
    - improve user flow by printing categories before asking user to select one
    - removed printer class

--- c-sharp/ConsoleApp1/Changelog.md
+++ c-sharp/ConsoleApp1/Changelog.md
@@ -14,12 +14,14 @@ The format is based on [Keep a Changelog][Keep a Changelog] and this project adh
 
 ### Changed
 
+- Removed printer class, for builtin Console.Writeline().
+- improved user flow by showing categories when user wants to filter a spcific category
 - changed jsonFeed to not be static
 
 ### Fixed
 
+- fix code smell, nested statements 6 levels down to 3 levels.
 - fix code smell, massive switch statement replaced with builtin c#
 - fix bug return only 1 joke
 - fix bug with assigning joke category
 - fix bug retrieving categories
-
--- c-sharp/ConsoleApp1/Program.cs
+++ c-sharp/ConsoleApp1/Program.cs
@@ -13,52 +13,44 @@ namespace ConsoleApp1
         static string[] results = new string[50];
         static char key;
         static Tuple<string, string> names;
-        static ConsolePrinter printer = new ConsolePrinter();
 
         static void Main(string[] args)
         {
-            printer.Value("Press ? to get instructions.").ToString();
+            Console.WriteLine("Press ? to get instructions.");
             if (Console.ReadLine() == "?")
             {
+                string category = null;
+
                 while (true)
                 {
-                    printer.Value("Press c to get categories").ToString();
-                    printer.Value("Press r to get random jokes").ToString();
+
+                    Console.WriteLine("Press any key to get random jokes");
+                    Console.ReadKey();
+
+                    Console.WriteLine("Want to use a random name? y/n");
                     GetEnteredKey(Console.ReadKey());
-                    if (key == 'c')
+
+                    if (key == 'y')
+                        GetNames();
+
+                    Console.WriteLine("Want to specify a category? y/n");
+                    GetEnteredKey(Console.ReadKey());
+
+                    if (key == 'y')
                     {
+                        Console.WriteLine("Loading available categories... (be patient)");
                         GetCategories();
+
+                        Console.WriteLine("Enter a category, then press Enter");
+                        category = Console.ReadLine();
+
                     }
-                    if (key == 'r')
-                    {
-                        
-                        printer.Value("Want to use a random name? y/n").ToString();
-                        GetEnteredKey(Console.ReadKey());
-                        
-                        if (key == 'y')
-                            GetNames();
 
-                        printer.Value("Want to specify a category? y/n").ToString();
-                        GetEnteredKey(Console.ReadKey());
+                    Console.WriteLine("How many jokes do you want? (1-9), then press Enter");
+                    int n = Int32.Parse(Console.ReadLine());
+                    GetRandomJokes(category, n);
 
-                        if (key == 'y')
-                        {
-                            printer.Value("How many jokes do you want? (1-9)").ToString();
-                            int n = Int32.Parse(Console.ReadLine());
-
-                            printer.Value("Enter a category;").ToString();
-                            GetRandomJokes(Console.ReadLine(), n);
-                        }
-                        else
-                        {
-                            
-                            printer.Value("How many jokes do you want? (1-9)").ToString();
-                            int n = Int32.Parse(Console.ReadLine());
-                            
-                            GetRandomJokes(null, n);
-
-                        }
-                    }
+                    category = null;
                     names = null;
                 }
             }
